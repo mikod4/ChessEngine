21213dc2efbc0290f49a48f96e701a50205a0e92
mikod4 <mik.domar4@gmail.com>
2026-05-27 21:23:52 +0200

    Icons fix
    This reverts commit 65e938bec04fd64cd6575bb2d20068cbcfd10cb2.

--- src/views/piece.py
+++ src/views/piece.py
@@ -1,9 +1,11 @@
 from PyQt6.QtSvgWidgets import QGraphicsSvgItem
 from src.utils.constants import SQUARE_SIZE, PIECES_ICONS
 
+
 class Piece(QGraphicsSvgItem):
     def __init__(self, piece_char, row, col):
-        super().__init__(f"{PIECES_ICONS}/{piece_char}.svg")
+        super().__init__(
+            f"{PIECES_ICONS}/{"white" if piece_char.isupper() else "black"}/{piece_char}.svg")
 
         bounds = self.boundingRect()
         scale = SQUARE_SIZE / max(bounds.width(), bounds.height())
